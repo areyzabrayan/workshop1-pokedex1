d7d63bfa1b5f469f89ab9b5d71a7be7c88db1d81
Brayan Areiza <areizabrayan11@gmail.com>
2023-06-23 14:21:34 -0500

    first commit workshop1

---
+++ src/app/index.js
@@ -0,0 +1,13 @@
+import "./styles/style.scss";
+
+import bolaPokemon2 from '../app/images/bolaPokemon2.png';
+import fuegoRojo from '../app/images/fuegorojo.webp';
+// import pokemon3 from '../app/images/pokemon3.png';
+
+  const firstImage = document.querySelector(".imagen-header");
+  firstImage.src = bolaPokemon2;
+  const seconImage = document.querySelector(".imagen-section-a");
+  seconImage.src = fuegoRojo;
+  // const threeImage = document.querySelector(".imagen-section-b");
+  // threeImage.src = pokemon3;
+
