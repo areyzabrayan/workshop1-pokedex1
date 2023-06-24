855b96daa6a3380de09fb6dde866b3862ab4ab17
Brayan Areiza <areizabrayan11@gmail.com>
2023-06-24 15:59:49 -0500

    new actulization

--- src/app/index.js
+++ src/app/index.js
@@ -2,6 +2,8 @@ import "./styles/style.scss";
 
 import bolaPokemon2 from '../app/images/bolaPokemon2.png';
 import fuegoRojo from '../app/images/fuegorojo.webp';
+import pikachu from '../app/images/pokemon4.png';
+import Eevee from '../app/images/Eevee.webp';
 // import pokemon3 from '../app/images/pokemon3.png';
 
   const firstImage = document.querySelector(".imagen-header");
@@ -10,4 +12,8 @@ import fuegoRojo from '../app/images/fuegorojo.webp';
   seconImage.src = fuegoRojo;
   // const threeImage = document.querySelector(".imagen-section-b");
   // threeImage.src = pokemon3;
+  const fourImage = document.querySelector(".pokemon");
+  fourImage.src = pikachu;
+  const fiveImage = document.querySelector(".pokemonF");
+  fiveImage.src = Eevee;
 
---
+++ src/app/scripts/services/dataPokemons.js
@@ -0,0 +1,3 @@
+export const URL_API = 'https://pokeapi.co/api/v2/pokemon/?offset=1000&limit=1000';
+
+ 
